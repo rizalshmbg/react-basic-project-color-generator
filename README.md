2fd5efa37f9dec4eb5c2f76b570f2cfd389da1ee
rizalshmbg <sihombingrizal18@gmail.com>
2022-10-07 19:33:20 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Basic Project : Color Generator
+
+## Idea : -
+
+### [Link](https://react-basic-project-color-generator.netlify.app)
+
+#### Create using useState, useEffect, Forms, and [Values JS](https://github.com/noeldelgado/values.js)
---
+++ src/SingleColor.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import rgbToHex from './utils'
+
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+	const [alert, setAlert] = useState(false)
+
+	const bcg = rgb.join(',')
+	const hex = rgbToHex(...rgb)
+	const hexValue = `#${hexColor.toUpperCase()}`
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setAlert(false)
+		}, 1500)
+		return () => clearTimeout(timeout)
+	}, [alert])
+
+	const copyToClipboard = () => {
+		setAlert(true)
+		navigator.clipboard.writeText(hexValue)
+	}
+
+	return (
+		<article
+			className={`color ${index > 10 && 'color-light'}`}
+			style={{ backgroundColor: `rgb(${bcg})` }}
+			onClick={copyToClipboard}
+		>
+			<p className='percent-value'>{weight}%</p>
+			<p className='color-value'>{hexValue}</p>
+			{alert && <p className='alert'>copied to clipboard!</p>}
+		</article>
+	)
+}
+
+export default SingleColor
